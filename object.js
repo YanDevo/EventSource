a4d873ce7b5dfe8021ed0a4e0a04b124841433be
1YanDevo <wadeyannick@gmail.com>
2022-09-05 20:44:00 -0500

    object.js

---
+++ object.js
@@ -0,0 +1,56 @@
+
+
+
+
+                                                                            // Destructure Assignment Syntax
+
+// // 1- destructure from Object Variable with const
+// const objOne = {
+//     a: 'dataA',
+//     b: 'datab'
+// };
+
+// //destructure
+// const {a, b} = objOne;
+
+// console.log(a)
+// console.log(b)
+
+
+
+
+
+// // 2- destructure from Object Variable with let
+
+// let objTwo = {
+//     c: 'dataC',
+//     d: 'dataD',
+// };
+
+// //destructure
+
+// let {c, d} = objTwo;
+
+// console.log(c);
+// console.log(d);
+
+
+
+
+// //3- destructuure from Object Literal with const
+
+// const {e, f} = {e: 'dataE', f: 'dataF'};
+
+// console.log(e);
+// console.log(f);
+
+
+
+
+// // 4- destructure from Object Literal with let
+
+// const {g, h} = {g: 'dataG', h: 'dataH'};
+
+// console.log(g);
+// console.log(h)
+ 
